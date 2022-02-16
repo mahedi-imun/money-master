d1eed7c9711eaea7921a0041d1f3bebaec814566
mahedi <mahedianik4@gmail.com>
2022-02-17 01:29:30 +0600

    complete devolop ui by boostrap add add js file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('hello')
+document.getElementById('calc-button').addEventListener('click',function(e){
+    // get income input value 
+    let income = document.getElementById('income-input');
+    let incomeValue = parseFloat(income.value);
+    
+
+
+
+
+
+   let totalExpenses= document.getElementById('total-expenses');
+   let totalExpensesValue = parseFloat(totalExpenses.innerText);
+//    console.log(totalExpensesValue)
+})
