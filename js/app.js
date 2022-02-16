c45eefd4c7ebadc8ec994fcb12b21325826ea648
mahedi <mahedianik4@gmail.com>
2022-02-17 05:49:54 +0600

    advence error handle

--- js/app.js
+++ js/app.js
@@ -11,62 +11,68 @@ function calculateInputValue() {
   // get others input value
   let others = document.getElementById("others-input");
   let othersValue = parseFloat(others.value);
-    // negative number error handle 
-  if( incomeValue > 0 && foodValue >0 && rentValue > 0 && othersValue > 0 ){
-        // get Total Expenses money
+  // negative number error handle
+  if (incomeValue > 0 && foodValue > 0 && rentValue > 0 && othersValue > 0) {
+    // get Total Expenses money
     let expensesMoney = foodValue + rentValue + othersValue;
     //
-    if( incomeValue > expensesMoney ){
-    // set expenses value
-    document.getElementById("total-expenses").innerText = expensesMoney;
-    let ballance = incomeValue - expensesMoney;
-    document.getElementById("ballance").innerText = ballance;
-    // hide error massage
-    document.getElementById('income-error').style.display='none';
-    document.getElementById('negative-num-error').style.display='none';
+    if (incomeValue > expensesMoney) {
+      // set expenses value
+      document.getElementById("total-expenses").innerText = expensesMoney;
+      let ballance = incomeValue - expensesMoney;
+      document.getElementById("ballance").innerText = ballance;
+      // hide error massage
+      document.getElementById("income-error").style.display = "none";
+      document.getElementById("negative-num-error").style.display = "none";
     }
     // show Expenditure is more than income error massage
-    else{ 
-        document.getElementById('income-error').style.display='block';
-    };
+    else {
+      document.getElementById("income-error").style.display = "block";
+    }
   }
-  // show negative number error massage 
-  else{
-    document.getElementById('negative-num-error').style.display='block';
-  };
- 
+  // show negative number error massage
+  else {
+    document.getElementById("negative-num-error").style.display = "block";
+  }
+}
 
-};
-
-// event handler function 
-function eventHandler(button){
-    document.getElementById(button).addEventListener("click", function (e) {
-        calculateInputValue();
-      });
-      
-};
+// event handler function
+function eventHandler(button) {
+  document.getElementById(button).addEventListener("click", function (e) {
+    calculateInputValue();
+  });
+}
 // call event handler function
 eventHandler("calc-button");
 
-
-document.getElementById('save-button').addEventListener("click",function(e){
+document.getElementById("save-button").addEventListener("click", function (e) {
+  // save money
+  let income = document.getElementById("income-input");
+  let incomeValue = parseFloat(income.value);
+  let balance = document.getElementById("ballance");
+  let ballanceNumber = parseFloat(balance.innerText);
+  let saveInput = document.getElementById("save-input");
+  let saveInputNumber = parseFloat(saveInput.value);
+  let save = (incomeValue * saveInputNumber) / 100;
+  // check error 
+  if (save >= 0) {
     
-        // save money 
-        
-        let income = document.getElementById("income-input");
-        let incomeValue = parseFloat(income.value);
-        let saveInput = document.getElementById('save-input');
-        let saveInputNumber = parseFloat(saveInput.value)
-        let save = incomeValue * saveInputNumber /100
-       let totalSave =  document.getElementById('saving-money').innerText=save;
-       let balance = document.getElementById("ballance");
-       let ballanceNumber = parseFloat(balance.innerText);
-       let RemainingBalance = ballanceNumber - totalSave;
-       document.getElementById('remaining-balance').innerText= RemainingBalance;
-
-
-
-   
+   if( ballanceNumber > save){
+    let totalSave = (document.getElementById("saving-money").innerText = save);
+    let RemainingBalance = ballanceNumber - totalSave;
+    document.getElementById("remaining-balance").innerText = RemainingBalance;
+    // hide negative number error massage 
+    document.getElementById('save-input-error').style.display="none"
+    // hide save more than ballance error massage 
+    document.getElementById('save-more-error').style.display="none"
+   }
+   // save ballance more than main ballance show error massage   
+   else{
+    document.getElementById('save-more-error').style.display="block"
+   }
+   // negative number show  error massage 
+  } 
+  else {
+      document.getElementById('save-input-error').style.display="block"
+  }
 });
-
-
